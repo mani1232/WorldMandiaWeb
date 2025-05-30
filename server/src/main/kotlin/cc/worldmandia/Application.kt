bd6fa30ac0a68c4dd0282d9a639bff864057bd0d
mani1232 <support@worldmandia.cc>
2025-05-31 01:38:29 +0200

    Add steam auth

--- server/src/main/kotlin/cc/worldmandia/Application.kt
+++ server/src/main/kotlin/cc/worldmandia/Application.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
 import io.ktor.server.http.content.*
+import io.ktor.server.response.respondText
 import io.ktor.server.routing.*
 import java.nio.file.Files
 import kotlin.io.path.Path
@@ -25,6 +26,22 @@ fun Application.module() {
             ignoreFiles {
                 it.endsWith(".txt")
             }
+        }
+        get("/signin-steam") {
+            val params = call.request.queryParameters
+
+            val state = params["state"]
+
+            val openidMode = params["openid.mode"]
+            val openidClaimedId = params["openid.claimed_id"]
+
+            call.respondText("""
+                    State: $state
+                    openid.mode: $openidMode
+                    openid.claimed_id: $openidClaimedId
+                    Все параметры:
+                    ${params.entries().joinToString("\n") { "${it.key} = ${it.value.joinToString()}" }}
+                """.trimIndent())
         }
     }
 }
